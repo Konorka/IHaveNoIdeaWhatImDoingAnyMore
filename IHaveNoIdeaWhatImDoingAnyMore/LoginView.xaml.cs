38a898ae8d664f2c2f826c17652fd8087d0f5bb1
Konorka <konor@hotmail.hu>
2018-02-05 14:54:01 +0100

    wat

---
+++ IHaveNoIdeaWhatImDoingAnyMore/LoginView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IHaveNoIdeaWhatImDoingAnyMore
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        
+        public LoginViewModel ViewModel { get; }
+        public LoginView()
+        {
+            
+            InitializeComponent();
+            ViewModel = new LoginViewModel();
+            DataContext = ViewModel;
+        }
+
+        private void LoginBtnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow _mw = new MainWindow();
+            ViewModel.Password = passwordPasswordBox.Password;
+            if (ViewModel.Login())
+            {
+                Close();
+                _mw.Show();
+            }
+            else
+                MessageBox.Show("Nem megfelelő adatok");
+        }
+    }
+}
---
+++ IHaveNoIdeaWhatImDoingAnyMore/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHaveNoIdeaWhatImDoingAnyMore
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User AuthenticatedUser { get; private set; }
+
+        public bool Login()
+        {
+            var manager = new DataManager();
+            var user = manager.GetUser(Username, Password);
+            if (user == null)
+                return false;
+            AuthenticatedUser = new User(user);
+            return true;
+        }
+    }
+}
